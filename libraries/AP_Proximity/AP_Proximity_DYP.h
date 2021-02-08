7d66378011ebd28f1581efc2d58213b0c20a08c4
wang-rh <40282094+wang-rh@users.noreply.github.com>
2021-02-08 12:10:57 +0800

    Create AP_Proximity_DYP.h

---
+++ libraries/AP_Proximity/AP_Proximity_DYP.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "AP_Proximity.h"
+#include "AP_Proximity_Backend.h"
+
+class AP_Proximity_DYP:public AP_Proximity_Backend
+{
+    public:
+        AP_Proximity_DYP(AP_Proximity &_frontend,AP_Proximity::Proximity_State &_state);
+
+        static bool detect();
+        void update(void)override;
+         // get maximum and minimum distances (in meters) of sensor
+        float distance_max() const override { return 4.5f; }
+        float distance_min() const override { return 0.30f; }
+        enum PARSE_phase{
+            STEP_head=0,
+            STEP_data
+        };
+    private:
+        void initialise();
+        void collect_byte(uint8_t msg);
+        void parserMsg();
+        void update_sector_distance(float angle,float distance);
+        enum PARSE_phase _parse_phase{STEP_head};
+        uint32_t _last_distance_received_ms;    // system time of last distance measurement received from sensor
+        AP_HAL::UARTDriver *_uart=nullptr;              // uart for communicating with sensor
+        uint8_t _data[9];
+        uint8_t _data_index=0;
+
+};
