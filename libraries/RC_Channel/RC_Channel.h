0d1fd4a219f331ac47185b89dbeb67a91e8cfe7e
wang-rh <40282094+wang-rh@users.noreply.github.com>
2021-03-08 17:21:13 +0800

    Update RC_Channel.h

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -183,6 +183,7 @@ public:
 
         // inputs eventually used to replace RCMAP
         MAINSAIL =           207, // mainsail input
+        REPEAT=             300,  //repeat mode
     };
     typedef enum AUX_FUNC aux_func_t;
 
