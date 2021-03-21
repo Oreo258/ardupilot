35a0782ea2c4e6027613e5de7a1aeec91de8be9d
wang-rh <40282094+wang-rh@users.noreply.github.com>
2021-03-21 13:20:07 +0800

    Update version.h

--- APMrover2/version.h
+++ APMrover2/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "HuaWei Special equipment"
+#define THISFIRMWARE "Unmanned Ground Vehicle"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,0,FIRMWARE_VERSION_TYPE_DEV
