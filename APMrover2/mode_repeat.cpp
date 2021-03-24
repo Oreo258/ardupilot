14504efe9f60bda3de593a3dad60230d721bd618
wang-rh <40282094+wang-rh@users.noreply.github.com>
2021-03-24 19:05:15 +0800

    Update mode_repeat.cpp

--- APMrover2/mode_repeat.cpp
+++ APMrover2/mode_repeat.cpp
@@ -10,27 +10,21 @@ bool ModeRepeat::_enter()
     if (!rover.mode_auto.mission.read_cmd_from_storage(1, cmd)) {
         return false;
     }
-
     // it's not a waypoint
     if (cmd.id != MAV_CMD_NAV_WAYPOINT) {
         return false;
     }
 
     // set destination
-   if (!g2.wp_nav.set_desired_location(cmd.content.location)) {
+    if (!g2.wp_nav.set_desired_location(cmd.content.location)) {
         return false;
     }
-
-    // copy current loc
-    origin_loc = rover.current_loc;
-
-    destination_loc = cmd.content.location; 
 
     // initialise waypoint speed
     g2.wp_nav.set_desired_speed_to_default();
 
-    round = false;
-    round_count = 0;
+    repeat = false;
+    repeat_count = 0;
     last_stop_time_ms = 0;
 
     return true;
@@ -43,8 +37,11 @@ void ModeRepeat::update()
         // update navigation controller
         navigate_to_waypoint();
     } else {
-        if (stop_vehicle()) {
-            if (round_count < g2.round_max) {
+
+        // we have reached the destination
+        // boats loiter, rovers stop
+    if (stop_vehicle()) {
+            if (repeat_count < g2.round_max) {
                 const uint32_t now_ms = AP_HAL::millis();
 
                 if (last_stop_time_ms == 0) {
@@ -52,15 +49,15 @@ void ModeRepeat::update()
                 }
 
                 if (now_ms - last_stop_time_ms > g2.round_delay * 1000) {
-                    if (!round) {
+                    if (!repeat) {
                         // set destination
                         if (!g2.wp_nav.set_desired_location(origin_loc)) {
                             //return;
                         }
                         set_reversed(true);
                     } else {
-                        gcs().send_text(MAV_SEVERITY_INFO, "Reapeat %d finished", unsigned(round_count + 1));
-                        if (++round_count < g2.round_max) {
+                        gcs().send_text(MAV_SEVERITY_INFO, "Repeat %d finished", unsigned(repeat_count + 1));
+                        if (++repeat_count < g2.round_max) {
                             // set destination
                             if (!g2.wp_nav.set_desired_location(destination_loc)) {
                                 //return;
@@ -68,11 +65,11 @@ void ModeRepeat::update()
                             set_reversed(false);
                         }
                     }
-                    	round = !round;
-                    	last_stop_time_ms = 0;
+                    repeat = !repeat;
+                    last_stop_time_ms = 0;
                 }
             }
-        }
+    }
 
         // update distance to destination
         _distance_to_destination = rover.current_loc.get_distance(g2.wp_nav.get_destination());
@@ -87,4 +84,14 @@ bool ModeRepeat::get_desired_location(Location& destination) const
         return true;
     }
     return false;
+}
+
+// set desired speed in m/s
+bool ModeRepeat::set_desired_speed(float speed)
+{
+    if (is_negative(speed)) {
+        return false;
+    }
+    g2.wp_nav.set_desired_speed(speed);
+    return true;
 }
