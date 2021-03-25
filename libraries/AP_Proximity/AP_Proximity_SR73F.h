86c416b1f96e9289fa2ccc75bd87c9cd50f79983
wang-rh <40282094+wang-rh@users.noreply.github.com>
2021-03-25 14:50:29 +0800

    Create AP_Proximity_SR73F.h

---
+++ libraries/AP_Proximity/AP_Proximity_SR73F.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "AP_Proximity.h"
+#include "AP_Proximity_Backend.h"
+
+class AP_Proximity_SR73F : public AP_Proximity_Backend
+{
+
+public:
+    // constructor
+    AP_Proximity_SR73F(AP_Proximity &_frontend, AP_Proximity::Proximity_State &_state);
+
+    // update state
+    void update(void) override;
+
+    // get maximum and minimum distances (in meters) of sensor
+    float distance_max() const override;
+    float distance_min() const override;
+
+    void update_sector(const float angle, const float dist, const uint8_t object_count) override;
+
+private:
+    bool initialise();
+     // horizontal distance support
+    bool _initialised;
+    uint32_t _last_update_ms;   // system time of last DISTANCE_SENSOR message received
+
+    float _angle_deg_last;
+    float _distance_m_last;
+    uint8_t   _last_sector;                   ///< last sector requested
+    float _angle_last[PROXIMITY_SECTORS_MAX];
+    float _distance_min_last[PROXIMITY_SECTORS_MAX];
+    bool _distance_valid_last[PROXIMITY_SECTORS_MAX];
+};
